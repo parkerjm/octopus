1eeb69d334095382270c23fdb38d7b3757539e77
James Parker <parkerjm@gmail.com>
2021-04-07 12:37:45 -0400

    1028: Add more privs to transformer user

--- priv/repo/migrations/20210407144041_add_readwrite_user.exs
+++ priv/repo/migrations/20210407144041_add_readwrite_user.exs
@@ -11,7 +11,9 @@ defmodule Octopus.Repo.Migrations.AddReadwriteUser do
       execute("GRANT CONNECT ON DATABASE #{database} TO #{username}")
       execute("GRANT USAGE ON SCHEMA public TO #{username}")
       execute("GRANT ALL ON ALL TABLES IN SCHEMA public TO #{username}")
+      execute("GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO #{username}")
       execute("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO #{username}")
+      execute("GRANT ALL ON DATABASE #{database} TO #{username}")
     end
   end
 
@@ -20,7 +22,9 @@ defmodule Octopus.Repo.Migrations.AddReadwriteUser do
       username = System.fetch_env!("DATABASE_TRANSFORMER_USERNAME")
       database = System.fetch_env!("DATABASE_NAME")
 
+      execute("REVOKE ALL ON DATABASE #{database} FROM #{username}")
       execute("ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM #{username}")
+      execute("REVOKE ALL ON ALL SEQUENCES IN SCHEMA public FROM #{username}")
       execute("REVOKE ALL ON ALL TABLES IN SCHEMA public FROM #{username}")
       execute("REVOKE USAGE ON SCHEMA public FROM #{username}")
       execute("REVOKE CONNECT ON DATABASE #{database} FROM #{username}")
