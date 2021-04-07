42f87f99e900a5348c61a423c2624677b68831d2
James Parker <parkerjm@gmail.com>
2021-04-07 11:03:26 -0400

    1028: Add a read/write transformer user

--- priv/repo/migrations/20210407144041_add_readwrite_user.exs
+++ priv/repo/migrations/20210407144041_add_readwrite_user.exs
@@ -1,30 +1,27 @@
-defmodule Octopus.Repo.Migrations.AddReadonlyUser do
+defmodule Octopus.Repo.Migrations.AddReadwriteUser do
   use Ecto.Migration
 
   def up do
     if Mix.env() == :prod do
-      username = System.fetch_env!("DATABASE_READONLY_USERNAME")
-      password = System.fetch_env!("DATABASE_READONLY_PASSWORD")
+      username = System.fetch_env!("DATABASE_TRANSFORMER_USERNAME")
+      password = System.fetch_env!("DATABASE_TRANSFORMER_PASSWORD")
       database = System.fetch_env!("DATABASE_NAME")
 
       execute("CREATE USER #{username} WITH PASSWORD '#{password}'")
       execute("GRANT CONNECT ON DATABASE #{database} TO #{username}")
       execute("GRANT USAGE ON SCHEMA public TO #{username}")
-      execute("GRANT SELECT ON ALL TABLES IN SCHEMA public TO #{username}")
-      execute("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO #{username}")
+      execute("GRANT ALL ON ALL TABLES IN SCHEMA public TO #{username}")
+      execute("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO #{username}")
     end
   end
 
   def down do
     if Mix.env() == :prod do
-      username = System.fetch_env!("DATABASE_READONLY_USERNAME")
+      username = System.fetch_env!("DATABASE_TRANSFORMER_USERNAME")
       database = System.fetch_env!("DATABASE_NAME")
 
-      execute(
-        "ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT ON TABLES FROM #{username}"
-      )
-
-      execute("REVOKE SELECT ON ALL TABLES IN SCHEMA public FROM #{username}")
+      execute("ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM #{username}")
+      execute("REVOKE ALL ON ALL TABLES IN SCHEMA public FROM #{username}")
       execute("REVOKE USAGE ON SCHEMA public FROM #{username}")
       execute("REVOKE CONNECT ON DATABASE #{database} FROM #{username}")
       execute("DROP USER #{username}")
