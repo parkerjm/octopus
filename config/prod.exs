3a48e847dac3d64ddbc04ee2112b05b1d9beec20
James Parker <parkerjm@gmail.com>
2021-05-11 13:43:40 -0400

    fix prod oban config

--- config/prod.exs
+++ config/prod.exs
@@ -6,34 +6,4 @@ config :octopus, OctopusWeb.Endpoint,
 
 config :logger, level: :info
 
-unique_opts = [
-  period: :infinity,
-  states: [:available, :scheduled, :executing, :retryable]
-]
-
-shared_crontab = [
-  {"*/15 * * * *", Octopus.Connector.Delighted, max_attempts: 1, unique: unique_opts},
-  {"*/15 * * * *", Octopus.Connector.RingCentral, max_attempts: 1, unique: unique_opts}
-]
-
-production_crontab = [
-  {"*/20 * * * *", Octopus.Connector.Domo, max_attempts: 1, unique: unique_opts},
-  {"*/30 * * * *", Octopus.Connector.Hubspot, max_attempts: 1, unique: unique_opts}
-]
-
-crontab =
-  if System.get_env("APP_ENV") == "production" do
-    shared_crontab ++ production_crontab
-  else
-    shared_crontab
-  end
-
-config :octopus, Oban,
-  repo: Octopus.Repo,
-  plugins: [
-    {Oban.Plugins.Pruner, max_age: 3600},
-    {Oban.Plugins.Cron, crontab: crontab}
-  ],
-  queues: [default: 10]
-
 # use runtime.exs for loading data from env
--- config/runtime.exs
+++ config/runtime.exs
@@ -22,6 +22,36 @@ if config_env() == :prod do
     pool_timeout: 60_000,
     timeout: 60_000
 
+  unique_opts = [
+    period: :infinity,
+    states: [:available, :scheduled, :executing, :retryable]
+  ]
+
+  shared_crontab = [
+    {"*/15 * * * *", Octopus.Connector.Delighted, max_attempts: 1, unique: unique_opts},
+    {"*/15 * * * *", Octopus.Connector.RingCentral, max_attempts: 1, unique: unique_opts}
+  ]
+
+  production_crontab = [
+    {"*/20 * * * *", Octopus.Connector.Domo, max_attempts: 1, unique: unique_opts},
+    {"*/30 * * * *", Octopus.Connector.Hubspot, max_attempts: 1, unique: unique_opts}
+  ]
+
+  crontab =
+    if System.get_env("APP_ENV") == "production" do
+      shared_crontab ++ production_crontab
+    else
+      shared_crontab
+    end
+
+  config :octopus, Oban,
+    repo: Octopus.Repo,
+    plugins: [
+      {Oban.Plugins.Pruner, max_age: 3600},
+      {Oban.Plugins.Cron, crontab: crontab}
+    ],
+    queues: [default: 10]
+
   config :octopus, :delighted,
     api_key: System.fetch_env!("DELIGHTED_API_KEY"),
     base_url: System.fetch_env!("DELIGHTED_BASE_URL"),
